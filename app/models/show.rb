070680f90808dab36e6ceaa7cbd1ae1621686c8b
Kulsum Shaikh <shaikhkulsum233@gmail.com>
2022-11-29 17:33:54 -0600

    commit tree d93500a898280c2a9dd70b6fcf2902d3723c905f

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,11 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network
+
+    def actors_list
+        actors.map  do |each_actor| 
+            "#{each_actor.first_name} #{each_actor.last_name}"
+        end
+    end
 end
